b21162f761f42bd303898d278bc142254542def0
oksananaumova <oksana.naumova@mustpay.tech>
2023-07-27 10:03:33 -0400

    Fizz Buzz answered

--- homework.js
+++ homework.js
@@ -1,14 +1,28 @@
-// Easy Going
+// // Easy Going
 
-for (let i = 1; i <= 20; i++) {
-    console.log(i);
-}
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
 
-// Get Even
+// // Get Even
 
-for (let i = 0; i <= 200; i++) {
+// for (let i = 0; i <= 200; i++) {
 
-    if (i % 2 === 0 && i !== 0) {
+//     if (i % 2 === 0 && i !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// Fizz Buzz
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
+        console.log('Fizz')
+    } else if (i % 5 === 0) {
+        console.log('Buzz')
+    } else {
         console.log(i);
     }
 }
